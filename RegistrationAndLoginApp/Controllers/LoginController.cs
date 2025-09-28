7173bd14ba5a1c673eb520595c9a56997433db06
Arsenije Brajovic <ABrajovic@my.gcu.edu>
2025-09-27 23:38:49 -0700

    Added Authentication and Authorization to Program.cs and updated the Login method to add claims

--- RegistrationAndLoginApp/Controllers/LoginController.cs
+++ RegistrationAndLoginApp/Controllers/LoginController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RegistrationAndLoginApp.Models.ViewModels;
 using RegistrationAndLoginApp.Services.BussinesLogicLayer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
 public class LoginController : Controller
 {
@@ -21,22 +24,56 @@ public class LoginController : Controller
 
     // POST: Handle login
     [HttpPost]
-   
-    public IActionResult Login(UserViewModel user)
+    public async Task<IActionResult> Login(UserViewModel user)
     {
         bool isValidated;
-        UserViewModel? validateUser;
+        UserViewModel? validatedUser;
 
-        (isValidated, validateUser) = _userLogic.ValidateUserCredentials(user.Username, user.Password);
+        (isValidated, validatedUser) = _userLogic.ValidateUserCredentials(user.Username, user.Password);
 
-        if (isValidated)
-            return View("LoginSuccess");
+        if (isValidated && validatedUser != null)
+        {
+            // Create claims
+            var claims = new List<Claim>
+            {
+                new Claim("UserId", validatedUser.Id.ToString()),
+                new Claim(ClaimTypes.Name, validatedUser.Username)
+            };
+
+            // Add roles/groups if any
+            if (validatedUser.GroupPossibilities != null)
+            {
+                foreach (var group in validatedUser.GroupPossibilities)
+                {
+                    if (group.IsSelected)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, group.Name));
+                    }
+                }
+            }
+
+            // Create identity & principal
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            // Sign in user and issue cookie
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                claimsPrincipal);
+
+            // Redirect to success page (forces navbar reload)
+            return RedirectToAction("LoginSuccess");
+        }
         else
         {
             ModelState.AddModelError("", "Invalid username or password");
-            return View(user); 
+            return View(user);
         }
     }
 
+    // GET: Login success page
+    public IActionResult LoginSuccess()
+    {
+        return View();
+    }
 }
-
--- RegistrationAndLoginApp/Program.cs
+++ RegistrationAndLoginApp/Program.cs
@@ -1,6 +1,7 @@
 using RegistrationAndLoginApp.Services.BussinesLogicLayer;
 using RegistrationAndLoginApp.Services.DataAccessLayer;
 using RegistrationAndLoginApp.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,20 @@ builder.Services.AddSingleton<IUserDAO, LocalUserDAO>();
 
 // Register UserLogic and let DI inject IUserDAO automatically
 builder.Services.AddScoped<UserLogic>();
+
+// Setup authentication and authorization
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        // Path to redirect when user is not authenticated
+        options.LoginPath = "/Login/Login";
+
+        // Path to redirect when user is forbidden (access denied)
+        options.AccessDeniedPath = "/Login/Login";
+    });
+
+// Enable authorization system
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -25,6 +40,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+// Make sure authentication runs before authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
